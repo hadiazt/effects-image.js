b69ccea62a7a78bb61e099e675cd6db0a20525eb
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-06 22:33:24 +0330

    🔔 | Main File

---
+++ methods.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Blur: require('./src/Blur'),
+    Greyscale: require('./src/Greyscale'),
+    Brightness: require('./src/Brightness'),
+    Rotate: require('./src/Rotate'),
+    Flip: require('./src/flip'),
+    Normalize: require('./src/Normalize'),
+    Invert: require('./src/Invert'),
+    Fisheye: require('./src/Fisheye'),
+    Circle: require('./src/Circle')
+};
